a70c61571a9fe52a3c4a827a7d74fca4cdb78106
sfikas0221 <stelios.fikas@gmail.com>
2019-04-12 14:55:45 -0500

    Added files of the project

---
+++ Conversion Calculator UI/Conversion Calculator UI/ConverterViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Conversion Calculator UI
+//
+//  Created by Stelios Fikas on 4/12/19.
+//  Copyright © 2019 Stelios Fikas. All rights reserved.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+    
+    // IB outlets and actions
+
+    @IBOutlet weak var outputDisplay: UITextField!
+    
+    
+    @IBOutlet weak var inputDisplay: UITextField!
+    
+    @IBAction func converterButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+
+
+}
+
